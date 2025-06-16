d6bdcf84ad676661c10c4a823a255d462d7a97e8
danilo-montes <dem9855@nyu.edu>
2025-06-16 19:13:06 -0400

    add function documentation

--- src/lunapyutils/__init__.py
+++ src/lunapyutils/__init__.py
@@ -1,9 +1,8 @@
-# python native
 from typing import Any
 
 
-# constants
 DEVELOPING = True
+
 
 
 def handle_error(
@@ -19,12 +18,14 @@ def handle_error(
     Parameters
     ----------
     error : Exception
-        the exception that was raised
+        The exception that was raised.
+
     function : str
-        the name of the function that the expection was raised in
+        The name of the function that the expection was raised in.
+
     default_error : str
-        the error message to be displayed to the user, non-technical
-        such that the user can more obviously know what to do
+        The error message to be displayed to the user, non-technical
+        such that the user can more obviously know what to do.
     """
 
     if DEVELOPING:
@@ -58,10 +59,11 @@ def print_internal(
     Parameters
     ----------
     text : Any
-        the data to display
+        The data to display.
+
     display_error_notice : bool, default=False
-        true if "[ERROR]" prefix is desired
-        false if "]" prefix is desired
+        True if "[ERROR]" prefix is desired.
+        False if "]" prefix is desired.
     """
 
     if display_error_notice:
@@ -78,7 +80,7 @@ def print_script_message(text : str) -> None:
     Parameters
     ----------
     text : str
-        the data to display
+        The data to display.
     """
 
     print(f'> {text}')
@@ -86,21 +88,63 @@ def print_script_message(text : str) -> None:
 
 
 def save_log(filename: str, data: Any) -> None:
+    """
+    Appends the given data to the given file.
+
+    Parameters
+    ----------
+    filename : str
+        The file path and name of the file to append to.
+
+    data : Any
+        The data to append to the file.
+    """
     with open(filename, 'a+', encoding="utf-8") as f:
         f.write(str(data))
+    
+    return
 
 
 def prompt_for_answer(prompt_text : str) -> str:
+    """
+    Prompts the user for an input to answer the given prompt.
+
+    Parameters
+    ----------
+    prompt_text : str
+        The prompt to give the user.
+
+        
+    Returns
+    -------
+    str
+        The answer the user inputted.
+    """
     answer = input(f'> {prompt_text}: ').strip()
 
     while not answer:
-        print('Please enter a valid string.')
+        print('Please enter a non-empty string.')
         answer = input(f'> {prompt_text}: ').strip()
 
     return answer
 
 
 def select_list_options(options : list[str]) -> int:
+    """
+    Prints a given list of options for the user to choose from by selecting
+    a number option.
+
+    Parameters
+    ----------
+    options : list[str]
+        The options for the user to choose from.
+
+        
+    Returns
+    -------
+    int
+        The integer indicating the chosen option.
+    """
 
     for i, option in enumerate(options):
         print(f'({i + 1}) {option}')
@@ -108,7 +152,7 @@ def select_list_options(options : list[str]) -> int:
     try:
         choice = int(input('] '))
 
-        if choice < 1 or choice > len(options):
+        if choice - 1 not in range(len(options)):
             raise IndexError
         
         print()
@@ -125,7 +169,29 @@ def select_list_options(options : list[str]) -> int:
 
 # method used
 # https://stackoverflow.com/a/7205107
-def merge_dicts(dict1: dict, dict2: dict, path=[]):
+def merge_dicts(dict1: dict, dict2: dict, path=[]) -> None:
+    """
+    Merges two dictionaries, storing the result in the first `dict` argument.
+
+    Paremeters
+    ----------
+    dict1 : dict
+        The first dictionary to merge.
+
+    dict2 : dict
+        The second dictionary to merge.
+
+
+    Raises
+    ------
+    Exception
+        If there is a conflict with keys between the two dictionaries.
+        A conflict occurs if both dictionaries have the same key, but the
+        types of values, or the values themselves, are different 
+        (ex. attempting to merge `{'count' : 1}` with `{'count' : 3}`
+        or with `{'count' : 'one'}`)
+    """
+    
     for key in dict2:
         if key in dict1:
             if isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
@@ -134,4 +200,5 @@ def merge_dicts(dict1: dict, dict2: dict, path=[]):
                 raise Exception('Conflict at ' + '.'.join(path + [str(key)]))
         else:
             dict1[key] = dict2[key]
+    
     return dict1
